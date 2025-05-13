da80a7ce0077002d0032f3b770aeb2910c7f144c
cmorcan602 <mccristian@iesrafaelalberti.es>
2025-05-13 17:39:38 +0200

    limpieza del main + commit inicial

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,10 +10,6 @@ import es.prog2425.ejerciciosBD9_1.service.ProductoService
 import es.prog2425.ejerciciosBD9_1.service.UsuarioService
 
 fun main() {
-    val nombre = "Cornelio Ramírez"
-    val precio = 24.99
-    val id = 3
-
     val usuarioDao = UsuarioDAOH2()
     val usuarioService = UsuarioService(usuarioDao)
     val productoDao = ProductoDAOH2()
@@ -22,11 +18,4 @@ fun main() {
     val pedidoService = PedidoService(pedidoDao)
     val lineaPedidoDao = LineaPedidoDAOH2()
     val lineaPedidoService = LineaPedidoService(lineaPedidoDao)
-
-    //Eliminar el usuario Cornelio
-    usuarioService.eliminarUsuario(nombre)
-    //Eliminar el producto de 24.99
-    productoService.eliminarProducto(precio)
-    //Eliminar el pedido de id 3
-    pedidoService.eliminarPedidoConLinea(id)
 }
