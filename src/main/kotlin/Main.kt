5f95a1558b1d122980b28f64ad516eeae3f93702
cmorcan602 <mccristian@iesrafaelalberti.es>
2025-05-17 18:17:03 +0200

    commit inicial

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,15 @@
 package es.prog2425.ejerciciosBD9_1
 
-import es.prog2425.ejerciciosBD9_1.data.dao.LineaPedidoDAOH2
-import es.prog2425.ejerciciosBD9_1.data.dao.PedidoDAOH2
-import es.prog2425.ejerciciosBD9_1.data.dao.ProductoDAOH2
-import es.prog2425.ejerciciosBD9_1.data.dao.UsuarioDAOH2
-import es.prog2425.ejerciciosBD9_1.service.LineaPedidoService
-import es.prog2425.ejerciciosBD9_1.service.PedidoService
-import es.prog2425.ejerciciosBD9_1.service.ProductoService
-import es.prog2425.ejerciciosBD9_1.service.UsuarioService
 import es.prog2425.ejerciciosBD9_1.data.db.DataSourceFactory
 import es.prog2425.ejerciciosBD9_1.data.db.Mode
-import es.prog2425.ejerciciosBD9_1.model.Usuario
+import es.prog2425.ejerciciosBD9_1.ui.UI
 
 fun main() {
+    val consola = UI()
+
     val dataSource = try {
         DataSourceFactory.getDataSource(Mode.HIKARI)
     } catch (e: IllegalStateException) {
         throw IllegalStateException("Problemas al crear el DataSource: ${e.message}")
     }
-
-    val usuario = Usuario(1, "Reinaldo Girúndez", "reingir@mail.com")
-    val nombreUsuario = "Facundo Pérez"
-
-    val usuarioDao = UsuarioDAOH2(dataSource)
-    val usuarioService = UsuarioService(usuarioDao)
-    val productoDao = ProductoDAOH2(dataSource)
-    val productoService = ProductoService(productoDao)
-    val pedidoDao = PedidoDAOH2(dataSource)
-    val pedidoService = PedidoService(pedidoDao)
-    val lineaPedidoDao = LineaPedidoDAOH2(dataSource)
-    val lineaPedidoService = LineaPedidoService(lineaPedidoDao)
-
-    usuarioService.addUsuario(usuario)
-    val pedidos = pedidoService.obtenerPedidosPorNombreUsuario(nombreUsuario)
-    println(pedidos)
 }
