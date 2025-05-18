323dde554ef0994a7886ee142a7f64cc8213fba7
cmorcan602 <mccristian@iesrafaelalberti.es>
2025-05-18 12:16:49 +0200

    principios de la consola creados

--- src/main/kotlin/ui/IEntradaSalida.kt
+++ src/main/kotlin/ui/IEntradaSalida.kt
@@ -1,4 +1,7 @@
 package es.prog2425.ejerciciosBD9_1.ui
 
 interface IEntradaSalida {
+    fun mostrarError(msj: String)
+    fun limpiarPantalla(lineas: Int)
+    fun pausa()
 }
--- src/main/kotlin/ui/UI.kt
+++ src/main/kotlin/ui/UI.kt
@@ -1,4 +1,20 @@
 package es.prog2425.ejerciciosBD9_1.ui
 
 class UI : IEntradaSalida {
+    override fun mostrarError(msj: String) {
+        println("*-_ERROR_-* $msj")
+    }
+
+    override fun limpiarPantalla(lineas: Int) {
+        var contador = 0
+        while (contador < lineas){
+            contador++
+            println("")
+        }
+    }
+
+    override fun pausa() {
+        println("Pulse ENTER...")
+        val valor = readln()
+    }
 }
