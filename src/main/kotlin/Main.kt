c36b74673d34553a3d5a936f9966c894b9f97ae6
cmorcan602 <mccristian@iesrafaelalberti.es>
2025-05-19 10:32:42 +0200

    cambio para ver si va el menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 package es.prog2425.ejerciciosBD9_1
 
+import es.prog2425.ejerciciosBD9_1.app.UsuariosManager
+import es.prog2425.ejerciciosBD9_1.data.dao.UsuarioDAOH2
 import es.prog2425.ejerciciosBD9_1.data.db.DataSourceFactory
 import es.prog2425.ejerciciosBD9_1.data.db.Mode
+import es.prog2425.ejerciciosBD9_1.service.UsuarioService
 import es.prog2425.ejerciciosBD9_1.ui.UI
 
 fun main() {
@@ -13,5 +16,8 @@ fun main() {
         throw IllegalStateException("Problemas al crear el DataSource: ${e.message}")
     }
 
-    
+    val usuarioDao = UsuarioDAOH2(dataSource)
+    val usuarioService = UsuarioService(usuarioDao)
+
+    UsuariosManager(usuarioService ,consola).programaUsuarios()
 }
