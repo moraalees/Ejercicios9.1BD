1f0d97a4d9848071667d69ff3a49acb97491c310
cmorcan602 <mccristian@iesrafaelalberti.es>
2025-05-21 16:03:30 +0200

    otra forma abrir cerrar conn

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,22 @@
 package es.prog2425.ejerciciosBD9_1
 
 import es.prog2425.ejerciciosBD9_1.data.db.DatabaseTienda
+import java.sql.SQLException
 
 fun main() {
-    val connection = DatabaseTienda.getConnection()
+    val connection = try{
+        DatabaseTienda.getConnection()
+    } catch (e: SQLException){
+        println("Se ha producido un error en la BD: ${e.message}")
+        null
+    } catch (e: Exception) {
+        println("Excepción inesperada: ${e.message}")
+        null
+    }
+    if (connection != null){
+        println("Conexión exitosa")
 
-    DatabaseTienda.closeConnection(connection)
+        DatabaseTienda.closeConnection(connection)
+        println("Se ha cerrado la conexión exitosamente.")
+    }
 }
